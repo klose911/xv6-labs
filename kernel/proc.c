10a036de66c3eadd1f9406e9134df6ef75d619bb
I514692 <klose.wu@sap.com>
2025-12-18 17:13:58 +0800

    init the alramframe

--- kernel/proc.c
+++ kernel/proc.c
@@ -146,6 +146,7 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  memset(&p->alarmframe, 0, sizeof(p->trapframe));
   p->alarm_handler = 0; // initialize alarm handler to null
   p->alarm_ticks = 0; 
   p->alarm_next = 0;
