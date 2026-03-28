00a9a6753cc92ea1c38186290c8a4aee012caf25
klose911 <klose911@gmail.com>
2026-03-28 21:24:45 +0800

    data structure of read write lock

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,4 +1,6 @@
 // Mutual exclusion lock.
+#include "types.h"
+
 struct spinlock {
   uint locked;       // Is the lock held?
 
@@ -6,8 +8,8 @@ struct spinlock {
   char *name;        // Name of lock.
   struct cpu *cpu;   // The cpu holding the lock.
 #ifdef LAB_LOCK
-  int nts;
-  int n;
+  int nts; // Number of calls to test-and-set.
+  int n; // Number of calls to acquire().
 #endif
 };
 
@@ -15,6 +17,15 @@ struct spinlock {
 // Reader-writer lock.
 struct rwspinlock {
   // Replace this with your implementation.
+
+  // condition:
+  // 1. read acquire: writer == 0 && waiting_writer == 0
+  // 2. write acquire: reader == 0 && writer == 0
+  // 3. read release: reader > 0; 
+  // 4. writer realse: writer > 0;  
+  uint reader;
+  uint writer;
+  uint waiting_writer;  
   struct spinlock l;
 };
 #endif
