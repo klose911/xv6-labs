e4b7729195bc3c0315d0e1663a5155653592d14d
I514692 <klose.wu@sap.com>
2026-04-03 16:23:16 +0800

    define the constant to support double indirect blocks

--- kernel/fs.h
+++ kernel/fs.h
@@ -1,6 +1,7 @@
 // On-disk file system format.
 // Both the kernel and user programs use this header file.
 
+#include "types.h"
 
 #define ROOTINO  1   // root i-number
 #define BSIZE 1024  // block size
@@ -24,9 +25,10 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define N_DOUBLE_INDIRECT (NINDIRECT * NINDIRECT) 
+#define MAXFILE (NDIRECT + NINDIRECT + N_DOUBLE_INDIRECT)
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +37,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+2];   // Data block addresses
 };
 
 // Inodes per block.
